9dc8f501322e15567090dd04b6dd4a0e3ff1d55e
Daniel Lombraña González <teleyinex@gmail.com>
2014-02-22 12:30:45 +0000

    First running version

---
+++ get_images.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of PyBOSSA.
+#
+# PyBOSSA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBOSSA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBOSSA.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+
+
+def get_iss_photos(start, n_photos, mission, size="small"):
+    """
+    Gets public photos from ISS missions
+    :arg string size: Size of the image from ISS mission
+    :returns: A list of photos.
+    :rtype: list
+    """
+    photos = []
+    for i in range(start, start + n_photos):
+        pattern = "http://eol.jsc.nasa.gov/sseop/images/ESC/%s/%s/%s-E-%s.JPG" % (
+            size,
+            mission,
+            mission,
+            i)
+        tmp = dict(link=pattern)
+        photos.append(tmp)
+    return photos
